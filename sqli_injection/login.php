8d1e2bd778381bac6e625561a6f636043994160d
Syed Asad Raza <111901981+AsadAnalyst@users.noreply.github.com>
2025-10-23 22:21:35 +0500

    Change database name from assignment_db to assignment_dbn

--- sqli_injection/login.php
+++ sqli_injection/login.php
@@ -5,7 +5,7 @@
 $db_host = "localhost";
 $db_user = "root";
 $db_pass = ""; // Default XAMPP password is empty
-$db_name = "assignment_db";
+$db_name = "assignment_dbn";
 
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
